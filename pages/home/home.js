8925dfb831464b9ed1e1bdfb3652836b370d91c9
mac <mac@landray.com.cn>
2023-07-14 21:38:02 +0800

    init: 项目初始化

---
+++ pages/home/home.js
@@ -0,0 +1,16 @@
+// f:\code_store\douyin\douyin_demo\pages\home\home.js
+Page({
+  data: {
+    PageCur: '粉丝福利'
+  },
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+  onLoad: function (options) {
+    // fanBenefits
+    // onlineCourses
+    // personalCenter
+  }
+})
