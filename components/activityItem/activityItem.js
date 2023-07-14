72ce2ba93ff6645769b047644e8a5a583302d5da
mac <2490572554@qq.com>
2023-07-14 22:50:26 +0800

    feat：学员/员工日常活动模块静态组件开发完成

---
+++ components/activityItem/activityItem.js
@@ -0,0 +1,24 @@
+// components/activityItem/activityItem.js
+Component({
+    properties: {
+        // 活动图片名称
+        img: {
+            type: String,
+            value: "t1"
+        },
+        // 活动名称
+        name:{
+            type:String,
+            value:""
+        }
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
--- pages/fanBenefits/fanBenefits.js
+++ pages/fanBenefits/fanBenefits.js
@@ -3,7 +3,17 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    // 活动列表
+    activities:[
+      {img:"t1",name:"学员日常活动"},
+      {img:"t2",name:"学员日常活动"},
+      {img:"t3",name:"学员日常活动"},
+      {img:"t1",name:"学员日常活动"},
+      {img:"t2",name:"学员日常活动"},
+      {img:"t3",name:"学员日常活动"},
+    ]
+  },
 
   /**
    * 组件的方法列表
