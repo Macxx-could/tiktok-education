ccaceae8371c81ad19695d619c5d7e8c94823ff8
mac <2490572554@qq.com>
2023-07-14 23:24:12 +0800

    feat：完成公司对外合作动态/新闻静态组件开发

---
+++ components/newsItem/newsItem.js
@@ -0,0 +1,18 @@
+// components/newsItem/newsItem.js
+Component({
+    properties: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
