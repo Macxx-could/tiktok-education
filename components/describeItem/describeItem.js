ef9d99b615f18be8aa7d268eeef221ba27cb7e99
mac <2490572554@qq.com>
2023-07-14 23:50:48 +0800

    feat：行业确认静态模块完成开发

---
+++ components/describeItem/describeItem.js
@@ -0,0 +1,29 @@
+// components/describeItem/describeItem.js
+Component({
+    properties: {
+        // 描述标题
+        title: {
+            type: String,
+            value: ""
+        },
+        // 描述内容
+        content: {
+            type: String,
+            value: ""
+        },
+        // 描述图片
+        img: {
+            type: String,
+            value: "t5"
+        }
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
