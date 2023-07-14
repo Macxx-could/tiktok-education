dd6339b2e16cb575f672bb85bfa34a8c71e94d96
mac <2490572554@qq.com>
2023-07-14 22:19:45 +0800

    feat：容器块组件开发完成

---
+++ components/containerBlock/containerBlock.js
@@ -0,0 +1,19 @@
+// components/containerBlock/containerBlock.js
+Component({
+    properties: {
+        // 容器标题
+        title: {
+            type: String,
+            value: ""
+        }
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
+})
