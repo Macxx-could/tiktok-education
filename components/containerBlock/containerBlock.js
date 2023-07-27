8886fe2222fc0b82ea4c8c5ecabbf566f43219c2
mac <2490572554@qq.com>
2023-07-27 23:57:18 +0800

    feat：containerBlock组件新增页面跳转功能

--- components/containerBlock/containerBlock.js
+++ components/containerBlock/containerBlock.js
@@ -15,5 +15,21 @@ Component({
     /**
      * 组件的方法列表
      */
-    methods: {}
+    methods: {
+        /**
+         * 更多文字点击事件
+         */
+        clickMoreText(e) {
+            const {currentTarget: {dataset: {title}}} = e
+            // 路径映射
+            const urlMap = {
+                ['学员/员工日常活动']: "/pages/newsList/newsList",
+                ['公司对外合作动态/新闻']: "/pages/newsList/newsList",
+                ['行业前景确认']: "/pages/newsList/newsList",
+                ['创始人故事/品牌故事']: "/pages/newsList/newsList",
+            }
+            const url = urlMap[title] || "/pages/home/home"
+            tt.navigateTo({url})
+        }
+    }
 })
