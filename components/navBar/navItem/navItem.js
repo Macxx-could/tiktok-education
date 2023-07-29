4a100ab20cc131992814484cf388482eb84b5bd6
mac <2490572554@qq.com>
2023-07-29 18:25:24 +0800

    feat：完成企业宣传片静态模块开发

--- components/navBar/navItem/navItem.js
+++ components/navBar/navItem/navItem.js
@@ -18,5 +18,17 @@ Component({
     /**
      * 组件的方法列表
      */
-    methods: {}
+    methods: {
+        clickItem() {
+            const key = this.data.text
+            const urlMap = {
+                ['企业介绍']: "/pages/companiesToIntroduce/companiesToIntroduce",
+                ['课程介绍']: "/pages/companiesToIntroduce/companiesToIntroduce",
+                ['学员成果']: "/pages/companiesToIntroduce/companiesToIntroduce",
+                ['线上课程']: "/pages/companiesToIntroduce/companiesToIntroduce",
+            }
+            const url = urlMap[key] || "/pages/home/home"
+            tt.navigateTo({url})
+        }
+    }
 })
