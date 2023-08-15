044d883bf73c9e17444ce653474a1eec26cfd0d1
mac <2490572554@qq.com>
2023-08-15 22:02:38 +0800

    feat：播报接口对接完成

--- components/notice/notice.js
+++ components/notice/notice.js
@@ -1,10 +1,47 @@
-// components/notice/notice.js 
+const { request } = require('../../utils/request.js');
 Component({
     /**
      * 组件的初始数据
      */
-    data: {},
+    data: {
+        list: [], // 播报列表
+        current: 0, // 当前播报内容索引
+        timer: null,
+        content: ""
+    },
+    /**
+     * 生命周期
+     */
+    lifetimes: {
+        attached() {
+            // 获取播报列表
+            const url = '/notice/list'
+            request(url, 'get').then(res => {
+                const { data: list } = res
+                this.setData({ list, current: 0 })
+                if (list && list.length > 0) {
+                    this.setData({
+                        content: list[0].content
+                    })
+                    const timer = setInterval(() => {
+                        let current = ++this.data.current
+                        if (current >= this.data.list.length) {
+                            current = 0
+                        }
+                        const { content } = this.data.list[current]
+                        this.setData({ current, content })
+                    }, 3000)
+                    this.setData({ timer })
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+        },
 
+    },
+    observers: {
+
+    },
     /**
      * 组件的方法列表
      */
