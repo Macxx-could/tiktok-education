6b2054370709fd51cdbceb2efb4a26f1843b4548
mac <2490572554@qq.com>
2023-08-18 16:41:23 +0800

    feat：校区环境接口对接完成

--- pages/environmentList/environmentList.js
+++ pages/environmentList/environmentList.js
@@ -1,23 +1,11 @@
-// pages/newsList/newsList.js
+const { request } = require('../../utils/request.js');
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     // 公司对外合作动态/新闻
-    environment: [
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-      { name: "广东校区环境一角" },
-    ],
+    environment: [],
     currentPage: 1,
     pageSize: 10,
     total: 100,
@@ -26,20 +14,38 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function (options) {
+    this.getEnvironment()
+  },
+  // 获取行业发展前景
+  getEnvironment() {
+    const url = '/article/page'
+    request(url, 'get', {
+      dyMenuId: 11,
+      pageNum: this.data.currentPage,
+      pageSize: this.data.pageSize
+    }).then(res => {
+      const { code, rows: environment, total } = res
+      if (code === 200) {
+        this.setData({ environment, total })
+      }
+    })
+  },
   handlePageChange(event) {
     const page = event.detail.page;
     this.setData({
       currentPage: page,
     });
     // 在这里可以根据新的页码进行数据的加载或其他操作
+    this.getEnvironment()
   },
   /**
    * 跳转到环境详情页面
    * @param {*} e
    */
   jumpToDetail(e) {
-    const url = `/pages/environmentalDetails/environmentalDetails`;
+    const { currentTarget: { dataset: { id } } } = e
+    const url = `/pages/environmentalDetails/environmentalDetails?id=${id}`;
     tt.navigateTo({ url });
   },
 });
--- pages/environmentalDetails/environmentalDetails.js
+++ pages/environmentalDetails/environmentalDetails.js
@@ -1,9 +1,26 @@
-// f:\code\part_time_job\tiktok-education\pages\environmentalDetails\environmentalDetails.js
+const { request } = require('../../utils/request.js');
 Page({
+  /**
+ * 页面的初始数据
+ */
   data: {
-
+    detail: {}
   },
+  /**
+     * 生命周期函数--监听页面加载
+     */
   onLoad: function (options) {
-
+    const { id } = options
+    this.getDetail(id)
+  },
+  // 获取详情
+  getDetail(id) {
+    const url = '/article/get/one'
+    request(url, 'get', { id }).then(res => {
+      const { code, data: detail } = res
+      if (code === 200) {
+        this.setData({ detail })
+      }
+    })
   }
 })
