8ca56613a7d7f9c7807e38a1e63baca6437db931
mac <2490572554@qq.com>
2023-08-18 17:13:19 +0800

    feat：粉丝福利模块完成

--- pages/fanBenefits/fanBenefits.js
+++ pages/fanBenefits/fanBenefits.js
@@ -1,26 +1,33 @@
-// components/test.js
+const { request } = require('../../utils/request.js');
 Component({
     /**
      * 组件的初始数据
      */
     data: {
-        list: [{
-            name: '领取福利一',
-        }, {
-            name: '领取福利一',
-        }, {
-            name: '领取福利一',
-        }, {
-            name: '领取福利一',
-        }, {
-            name: '领取福利一',
-        }, {
-            name: '领取福利一',
-        }]
+        detail: {}
     },
-
+    lifetimes: {
+        attached() {
+            this.getBenefits()
+        }
+    },
     /**
      * 组件的方法列表
      */
-    methods: {}
+    methods: {
+        getBenefits() {
+            const url = '/article/page'
+            request(url, 'get', { dyMenuId: 20 }).then(res => {
+                const { code, rows } = res
+                if (code === 200 && Array.isArray(rows) && rows.length > 0) {
+                    // 获取第一个元素
+                    const { id } = rows[0]
+                    const url = `/article/get/one`
+                    request(url, 'get', { id }).then(res => {
+                        this.setData({ detail: res.data })
+                    })
+                }
+            })
+        }
+    }
 });
